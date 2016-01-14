4ee8d4d0662ef30514fd582590d88073d8fd56ef
learnin9 <learnin9@gmail.com>
2016-01-14 18:08:37 +0800

    Create axel_install.sh

---
+++ simple-installer/axel_install.sh
@@ -0,0 +1,8 @@
+3  下载配置文件axelget.conf与axelget.py到yum里：
+cd /etc/yum/pluginconf.d/
+wget http://cnfreesoft.googlecode.com/svn/trunk/axelget/axelget.conf
+cd /usr/lib/yum-plugins/
+wget http://cnfreesoft.googlecode.com/svn/trunk/axelget/axelget.py
+最后确认 /etc/yum.conf中plugins=1
+
+下载地址http://pkgs.repoforge.org/axel/ 
